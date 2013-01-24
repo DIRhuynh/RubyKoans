7d340df0cd7bee89b630b6bcbd347ea9a069a620
John Huynh <j.n.huynh@me.com>
2013-01-24 18:03:47 -0500

    Finished about_symbols.rb

--- about_symbols.rb
+++ about_symbols.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 class AboutSymbols < EdgeCase::Koan
   def test_symbols_are_symbols
     symbol = :ruby
-    assert_equal __, symbol.is_a?(Symbol)
+    assert_equal(true, symbol.is_a?(Symbol))
   end
 
   def test_symbols_can_be_compared
@@ -11,72 +11,90 @@ class AboutSymbols < EdgeCase::Koan
     symbol2 = :a_symbol
     symbol3 = :something_else
 
-    assert_equal __, symbol1 == symbol2
-    assert_equal __, symbol1 == symbol3
+    assert_equal(true, symbol1 == symbol2)
+    assert_equal(false, symbol1 == symbol3)
   end
 
   def test_identical_symbols_are_a_single_internal_object
     symbol1 = :a_symbol
     symbol2 = :a_symbol
 
-    assert_equal __, symbol1           == symbol2
-    assert_equal __, symbol1.object_id == symbol2.object_id
+    # Symbols are unique values so it makes sense to store them
+    # internally as the same object.
+    assert_equal(true, symbol1 == symbol2)
+    assert_equal(true, symbol1.object_id == symbol2.object_id)
   end
 
   def test_method_names_become_symbols
     symbols_as_strings = Symbol.all_symbols.map { |x| x.to_s }
-    assert_equal __, symbols_as_strings.include?("test_method_names_become_symbols")
+    # Each method must have a unique name, or it must be namespaced to prevent
+    # unintentional overloading. It makes sense to use symbols for this case.
+    # Also it makes sense that because of this if there is a conflicty, function
+    # overloading occurs.
+    assert_equal(true, symbols_as_strings.include?("test_method_names_become_symbols"))
   end
 
   # THINK ABOUT IT:
   #
   # Why do we convert the list of symbols to strings and then compare
   # against the string value rather than against symbols?
+  #
+  # - Symbols are stripped down versions of Strings and so do not have
+  #   all methods available to it that a String would.
+  # - We could also conver the method name string into a symbol and then
+  #   run the comparison with the array of symbols instead.
 
   in_ruby_version("mri") do
     RubyConstant = "What is the sound of one hand clapping?"
     def test_constants_become_symbols
       all_symbols = Symbol.all_symbols
 
-      assert_equal __, all_symbols.include?(__)
+      assert_equal(false, all_symbols.include?(RubyConstant.to_sym))
     end
   end
 
   def test_symbols_can_be_made_from_strings
     string = "catsAndDogs"
-    assert_equal __, string.to_sym
+    # :<some_characters> is a Symbol literal constructor.
+    assert_equal(:catsAndDogs, string.to_sym)
   end
 
   def test_symbols_with_spaces_can_be_built
+    # Didn't know this, that is funkayyyy.
     symbol = :"cats and dogs"
 
-    assert_equal symbol, __.to_sym
+    assert_equal(symbol, "cats and dogs".to_sym)
   end
 
   def test_symbols_with_interpolation_can_be_built
+    # WOAAAAHHHHHH!
     value = "and"
     symbol = :"cats #{value} dogs"
 
-    assert_equal symbol, __.to_sym
+    assert_equal(symbol, "cats #{value} dogs".to_sym)
   end
 
   def test_to_s_is_called_on_interpolated_symbols
     symbol = :cats
     string = "It is raining #{symbol} and dogs."
 
-    assert_equal __, string
+    # This must be because the interpolation calls Object#to_s
+    # for all variables interpolated.
+    assert_equal("It is raining cats and dogs.", string)
   end
 
   def test_symbols_are_not_strings
     symbol = :ruby
-    assert_equal __, symbol.is_a?(String)
-    assert_equal __, symbol.eql?("ruby")
+    assert_equal(false, symbol.is_a?(String))
+    assert_equal(false, symbol.eql?("ruby"))
   end
 
   def test_symbols_do_not_have_string_methods
     symbol = :not_a_string
-    assert_equal __, symbol.respond_to?(:each_char)
-    assert_equal __, symbol.respond_to?(:reverse)
+
+    # Boom, Object#respond_to? allows some meta magic yo.
+    assert_equal(false, symbol.respond_to?(:each_char))
+    assert_equal(false, symbol.respond_to?(:reverse))
   end
 
   # It's important to realize that symbols are not "immutable
@@ -85,16 +103,20 @@ class AboutSymbols < EdgeCase::Koan
 
   def test_symbols_cannot_be_concatenated
     # Exceptions will be pondered further down the path
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       :cats + :dogs
     end
   end
 
   def test_symbols_can_be_dynamically_created
-    assert_equal __, ("cats" + "dogs").to_sym
+    assert_equal(:catsdogs, ("cats" + "dogs").to_sym)
   end
 
   # THINK ABOUT IT:
   #
   # Why is it not a good idea to dynamically create a lot of symbols?
+  #
+  # - Ton of reserved spaces can be taken up. Also if you dynamically create
+  #   symbols, you may end up creating one that would have been a method name
+  #   you need.
 end
