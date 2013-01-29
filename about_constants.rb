38f25eb5971ae62782738b7306c2550c20f93579
John Huynh <j.n.huynh@me.com>
2013-01-29 18:05:14 -0500

    Yay constants(more research) and control statements.

--- about_constants.rb
+++ about_constants.rb
@@ -1,5 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
+# Constants are defined outsite of methods in the class.
 C = "top level"
 
 class AboutConstants < EdgeCase::Koan
@@ -7,16 +8,19 @@ class AboutConstants < EdgeCase::Koan
   C = "nested"
 
   def test_nested_constants_may_also_be_referenced_with_relative_paths
-    assert_equal __, C
+    assert_equal("nested", C)
   end
 
   def test_top_level_constants_are_referenced_by_double_colons
-    assert_equal __, ::C
+    # ::<constant_variable_name>
+    assert_equal("top level", ::C)
   end
 
   def test_nested_constants_are_referenced_by_their_complete_path
-    assert_equal __, AboutConstants::C
-    assert_equal __, ::AboutConstants::C
+    # So in a class we can use the <Classname>::<constant_variable_name>
+    # The "::" is a namespace indicator... Is a class a namespace?
+    assert_equal("nested", AboutConstants::C)
+    assert_equal("nested", ::AboutConstants::C)
   end
 
   # ------------------------------------------------------------------
@@ -35,7 +39,7 @@ class AboutConstants < EdgeCase::Koan
   end
 
   def test_nested_classes_inherit_constants_from_enclosing_classes
-    assert_equal __, Animal::NestedAnimal.new.legs_in_nested_animal
+    assert_equal(4, Animal::NestedAnimal.new.legs_in_nested_animal)
   end
 
   # ------------------------------------------------------------------
@@ -47,7 +51,7 @@ class AboutConstants < EdgeCase::Koan
   end
 
   def test_subclasses_inherit_constants_from_parent_classes
-    assert_equal __, Reptile.new.legs_in_reptile
+    assert_equal(4, Reptile.new.legs_in_reptile)
   end
 
   # ------------------------------------------------------------------
@@ -63,11 +67,20 @@ class AboutConstants < EdgeCase::Koan
   end
 
   def test_who_wins_with_both_nested_and_inherited_constants
-    assert_equal __, MyAnimals::Bird.new.legs_in_bird
+    # It looks like the nested constant beat the inherited constant...
+    # Why?
+    assert_equal(2, MyAnimals::Bird.new.legs_in_bird)
   end
 
   # QUESTION: Which has precedence: The constant in the lexical scope,
   # or the constant from the inheritance hierarchy?
+  #
+  # I think the Inheritance Hierarchy has the precedence and so it is evaluated
+  # first. Then the Lexical Scope constant over writes the constant...?
+  #
+  # Well it is a constant so maybe we can't overwrite it. In that case my answer
+  # is reverse and the Lexical Scope constant has precedence and thus it is
+  # evaluated first.
 
   # ------------------------------------------------------------------
 
@@ -78,10 +91,16 @@ class AboutConstants < EdgeCase::Koan
   end
 
   def test_who_wins_with_explicit_scoping_on_class_definition
-    assert_equal __, MyAnimals::Oyster.new.legs_in_oyster
+    assert_equal(4, MyAnimals::Oyster.new.legs_in_oyster)
   end
 
   # QUESTION: Now which has precedence: The constant in the lexical
   # scope, or the constant from the inheritance hierarchy?  Why is it
   # different than the previous answer?
+  #
+  # The inheritance hierarchy has precedence...
+  #
+  # My guess is that this is because we are loading the Lexical Scope constant
+  # first before we inherit from the Animal Inheritance Hierarchy and thus the
+  # latter over write the former...
 end
--- about_control_statements.rb
+++ about_control_statements.rb
@@ -8,7 +8,7 @@ class AboutControlStatements < EdgeCase::Koan
     else
       result = :false_value
     end
-    assert_equal __, result
+    assert_equal(:true_value, result)
   end
 
   def test_if_then_statements
@@ -16,23 +16,26 @@ class AboutControlStatements < EdgeCase::Koan
     if true
       result = :true_value
     end
-    assert_equal __, result
+    assert_equal(:true_value, result)
   end
 
   def test_if_statements_return_values
+    # Cool, a clean way to initialize w.o. the ternary operator.
+    # If statements, returns the last expression evaluated in the
+    # construct.
     value = if true
               :true_value
             else
               :false_value
             end
-    assert_equal __, value
+    assert_equal(:true_value, value)
 
     value = if false
               :true_value
             else
               :false_value
             end
-    assert_equal __, value
+    assert_equal(:false_value, value)
 
     # NOTE: Actually, EVERY statement in Ruby will return a value, not
     # just if statements.
@@ -42,27 +45,36 @@ class AboutControlStatements < EdgeCase::Koan
     value = if false
               :true_value
             end
-    assert_equal __, value
+    # My mental image is that all if statements have a corresponding else.
+    # If the else is not explicity defined, then it is empty and thus returns
+    # a nil.
+    assert_equal(nil, value)
   end
 
   def test_condition_operators
-    assert_equal __, (true ? :true_value : :false_value)
-    assert_equal __, (false ? :true_value : :false_value)
+    # the "?" in <expression> ? <expression for true> : <expression for false> is
+    # called a ternary operator
+    assert_equal(:true_value, (true ? :true_value : :false_value))
+    assert_equal(:false_value, (false ? :true_value : :false_value))
   end
 
   def test_if_statement_modifiers
     result = :default_value
     result = :true_value if true
 
-    assert_equal __, result
+    assert_equal(:true_value, result)
   end
 
   def test_unless_statement
     result = :default_value
+    # This can be interpreted as, do everything in this construct unless the
+    # expression the "unless" prefixes evaluates to true.
+    #
+    # i.e. Do unless this is true.
     unless false    # same as saying 'if !false', which evaluates as 'if true'
       result = :false_value
     end
-    assert_equal __, result
+    assert_equal(:false_value, result)
   end
 
   def test_unless_statement_evaluate_true
@@ -70,14 +82,14 @@ class AboutControlStatements < EdgeCase::Koan
     unless true    # same as saying 'if !true', which evaluates as 'if false'
       result = :true_value
     end
-    assert_equal __, result
+    assert_equal(:default_value, result)
   end
 
   def test_unless_statement_modifier
     result = :default_value
     result = :false_value unless false
 
-    assert_equal __, result
+    assert_equal(:false_value, result)
   end
 
   def test_while_statement
@@ -87,28 +99,30 @@ class AboutControlStatements < EdgeCase::Koan
       result = result * i
       i += 1
     end
-    assert_equal __, result
+    assert_equal(3628800, result)
   end
 
   def test_break_statement
     i = 1
     result = 1
     while true
+      # Perform the break unless i <= 10. So when i = 11.
       break unless i <= 10
       result = result * i
       i += 1
     end
-    assert_equal __, result
+    assert_equal(3628800, result)
   end
 
   def test_break_statement_returns_values
     i = 1
     result = while i <= 10
+      # You need to pass break a parameter to return?
       break i if i % 2 == 0
       i += 1
     end
 
-    assert_equal __, result
+    assert_equal(2, result)
   end
 
   def test_next_statement
@@ -116,19 +130,21 @@ class AboutControlStatements < EdgeCase::Koan
     result = []
     while i < 10
       i += 1
+      # skip all remaining code after this next in this iteration of the loop.
       next if (i % 2) == 0
       result << i
     end
-    assert_equal __, result
+    assert_equal([1, 3, 5, 7, 9], result)
   end
 
   def test_for_statement
     array = ["fish", "and", "chips"]
     result = []
+    # This can be substitute with array.each { |item| result << item }
     for item in array
       result << item.upcase
     end
-    assert_equal [__, __, __], result
+    assert_equal(["FISH", "AND", "CHIPS"], result)
   end
 
 end
