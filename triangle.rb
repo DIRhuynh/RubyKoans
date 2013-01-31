40f36b67aaf0b964565b7bcdc8ce39c8dcacb350
John Huynh <j.n.huynh@me.com>
2013-01-31 18:09:52 -0500

    Finished exceptions for triangle and iteration.

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  # Verify that the dimensions are valid for a triangle.
+  raise TriangleError, "Side A (#{a}) invalid length." if a <= 0 || a >= (b + c)
+  raise TriangleError, "Side B (#{b}) invalid length." if b <= 0 || b >= (a + c)
+  raise TriangleError, "Side C (#{c}) invalid length." if c <= 0 || c >= (a + b)
+
   match_count = 0
 
   match_count = match_count + 1 if a == b
