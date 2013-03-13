c91546f7e0e5108475d09dd69c27e3b1e3c9d59d
John Huynh <j.n.huynh@me.com>
2013-03-13 18:56:12 -0400

    Finished scope... so close...

--- about_scope.rb
+++ about_scope.rb
@@ -18,7 +18,7 @@ class AboutScope < EdgeCase::Koan
   end
 
   def test_dog_is_not_available_in_the_current_scope
-    assert_raise(___) do
+    assert_raise(NameError) do
       fido = Dog.new
     end
   end
@@ -26,11 +26,13 @@ class AboutScope < EdgeCase::Koan
   def test_you_can_reference_nested_classes_using_the_scope_operator
     fido = Jims::Dog.new
     rover = Joes::Dog.new
-    assert_equal __, fido.identify
-    assert_equal __, rover.identify
+    assert_equal(:jims_dog, fido.identify)
+    assert_equal(:joes_dog, rover.identify)
 
-    assert_equal __, fido.class != rover.class
-    assert_equal __, Jims::Dog != Joes::Dog
+    # They are different because the class name include the scope.
+    assert_equal(true, fido.class != rover.class)
+    # The line below, performs the same comparison as the line above.
+    assert_equal(true, Jims::Dog != Joes::Dog)
   end
 
   # ------------------------------------------------------------------
@@ -38,42 +40,55 @@ class AboutScope < EdgeCase::Koan
   class String
   end
 
+  # AboutScope is a Class, which inherits Module, which explains their how
+  # Class has the scope operator.
   def test_bare_bones_class_names_assume_the_current_scope
-    assert_equal __, AboutScope::String == String
+    assert_equal(true, AboutScope::String == String)
   end
 
+  # This is because the system string does not have any scope whereas our custom
+  # string is within the AboutScope scope.
   def test_nested_string_is_not_the_same_as_the_system_string
-    assert_equal __, String == "HI".class
+    assert_equal(false, String == "HI".class)
   end
 
   def test_use_the_prefix_scope_operator_to_force_the_global_scope
-    assert_equal __, ::String == "HI".class
+    assert_equal(true, ::String == "HI".class)
   end
 
   # ------------------------------------------------------------------
 
   PI = 3.1416
 
+  # Does that mean all classes and modules are constants?
   def test_constants_are_defined_with_an_initial_uppercase_letter
-    assert_equal __, PI
+    assert_equal(3.1416, PI)
   end
 
   # ------------------------------------------------------------------
 
   MyString = ::String
 
+  # Well that answered my question above...
+  # Does that mean, the 'class' keyword is, essentially, a method that
+  # takes a constant as a argument, to access the instance of a particular
+  # Class object with that name, and a block to add new methods/instance
+  # variables(properties/attributes)?
   def test_class_names_are_just_constants
-    assert_equal __, MyString == ::String
-    assert_equal __, MyString == "HI".class
+    assert_equal(true, MyString == ::String)
+    assert_equal(true, MyString == "HI".class)
   end
 
   def test_constants_can_be_looked_up_explicitly
-    assert_equal __, PI == AboutScope.const_get("PI")
-    assert_equal __, MyString == AboutScope.const_get("MyString")
+    assert_equal(true, PI == AboutScope.const_get("PI"))
+    assert_equal(true, MyString == AboutScope.const_get("MyString"))
   end
 
   def test_you_can_get_a_list_of_constants_for_any_class_or_module
-    assert_equal __, Jims.constants
-    assert Object.constants.size > _n_
+    # Constants are stored as Symbols.
+    assert_equal([:Dog], Jims.constants)
+    # _n_ is 999999...
+    # assert Object.constants.size > _n_
+    assert Object.constants.size < _n_
   end
 end
