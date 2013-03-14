58dd9546f74558fa98ddb896ac84d0932b8e1d5e
John Huynh <j.n.huynh@me.com>
2013-03-13 20:17:51 -0400

    Created a sweet method_missing powered proxy class!
    YEAAAHHHHHH!!!!!!!!

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,12 +13,32 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
+
   def initialize(target_object)
-    @object = target_object
-    # ADD MORE CODE HERE
+    @object   = target_object
+    @messages = []
   end
 
-  # WRITE CODE HERE
+  def called?(method_name)
+    @messages.include?(method_name)
+  end
+
+  def number_of_times_called(method_name)
+    @messages.select { |name| name == method_name }.length
+  end
+
+  def method_missing(method_name, *args, &block)
+    @messages << method_name
+
+    # In the cases below, the splat operator destructures the Array
+    # into individual arguments.
+    if block
+      @object.send(method_name, *args, block)
+    else
+      @object.send(method_name, *args)
+    end
+  end
 end
 
 # The proxy object should pass the following Koan:
